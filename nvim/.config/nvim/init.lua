1ea2b0e38a1d7bb418bd747d54f8725a83e465fb
industriousrebel <moiz@industriousrebel.com>
2024-02-23 17:56:51 -0500

    Remved all the lsp stuff, doesn't do well with vscode

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -25,45 +25,9 @@ else
 		    vim.cmd('colorscheme kanagawa-wave')
 		end
 	    },
-	    {
-		'nvim-telescope/telescope.nvim',
-		tag = '0.1.5',
-		dependencies = {'nvim-lua/plenary.nvim'}
-	    },
-	    {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
-	    {
-		"ThePrimeagen/harpoon",
-		branch = "harpoon2",
-		requires = {'nvim-lua/plenary.nvim'}
-	    },
-	    'mbbill/undotree',
-	    'tpope/vim-fugitive',
-	}
-
-	local opt = {
-	    {
-		'VonHeikemen/lsp-zero.nvim',
-		branch = 'v3.x',
-		requires = {
-		    {'williamboman/mason.nvim'},
-		    {'williamboman/mason-lspconfig.nvim'},
-		    {'neovim/nvim-lspconfig'},
-		    {'hrsh7th/nvim-cmp'},
-		    {'hrsh7th/cmp-nvim-lsp'},
-		    {'L3MON4D3/LuaSnip'}
-		}
-	    },
 	}
 
 
 	require("lazy").setup(plugins,opts)
 
-	local config = require("nvim-treesitter.configs")
-	config.setup {
-	  ensure_installed = { "c", "lua", "vim", "vimdoc", "query" },
-	  sync_install = false,
-	  highlight = {
-	    enable = true,
-	  },
-	}
 end
